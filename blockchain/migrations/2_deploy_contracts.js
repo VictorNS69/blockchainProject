facb60fcdb58268f98b5d09f97624cadde548066
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-05-03 19:30:03 +0200

    Now address is saved in a file when deploy

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const SocialNetwork = artifacts.require("./SocialNetwork.sol")
+const fs = require("fs")
 
 module.exports = function(deployer) {
-    deployer.deploy(SocialNetwork);
+    deployer.deploy(SocialNetwork)
+    .then(()=> {
+        const file_path = "build/contracts/SocialNetwork-address"
+        fs.writeFileSync(file_path, SocialNetwork.address, function (err){
+            if (err) throw err
+            console.log("Address saved in", file_path)
+        })
+    })
 }
