332795c3f82b2a3c8be2c61a2e06fe735cbb91a3
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-05-24 15:21:33 +0200

    fixed #16

--- socialNetwork/userManager/blockchain_wrapper.py
+++ socialNetwork/userManager/blockchain_wrapper.py
@@ -2,14 +2,14 @@ import json
 from requests.exceptions import ConnectionError
 from web3 import Web3, HTTPProvider
 
-
 NODE_URL = "http://127.0.0.1:8545"
+
 
 class BlockchainWrapper:
     def __init__(self):
         try:
-            self._w3 = Web3(HTTPProvider(NODE_URL))
-            self._w3.eth.defaultAccount = self._w3.eth.accounts[0]
+            self.__w3 = Web3(HTTPProvider(NODE_URL))
+            self.__w3.eth.defaultAccount = self.__w3.eth.accounts[0]
         except ConnectionError:
             raise ConnectionError("Connection error")
 
@@ -19,29 +19,32 @@ class BlockchainWrapper:
         except IOError:
             raise IOError("Error reading contract address")
 
-        self._contract_address = Web3.toChecksumAddress(info_address)
+        self.__contract_address = Web3.toChecksumAddress(info_address)
         try:
             with open("../blockchain/build/contracts/SocialNetwork.json", 'r', encoding='utf-8') as abi:
                 info_json = json.load(abi)
         except IOError:
             raise IOError("Error reading contract ABI")
 
-        self._contract_abi = info_json["abi"]
-        self._socialNetwork_contract = self._w3.eth.contract(address=self._contract_address, abi=self._contract_abi)
+        self.__contract_abi = info_json["abi"]
+        self.__socialNetwork_contract = self.__w3.eth.contract(address=self.__contract_address, abi=self.__contract_abi)
 
-    def add_user(self, user_id):
-        assert user_id >= 0
+    def add_user(self, username):
         try:
-            tx_hash = self._socialNetwork_contract.functions.addUser(user_id).transact()
-            return self._w3.eth.waitForTransactionReceipt(tx_hash)
+            tx_hash = self.__socialNetwork_contract.functions.addUser(username).transact()
+            return self.__w3.eth.waitForTransactionReceipt(tx_hash)
         except ValueError:
             raise ValueError("User already exists in the SC")
 
+    def get_user_bytes(self, username):
+        tx_hash = self.__socialNetwork_contract.functions.getBytes(username).call()
+        return '0x' + tx_hash.hex()
+
     def get_contract_address(self):
-        return self._contract_address
+        return self.__contract_address
 
     def get_contract_abi(self):
-        return self._contract_abi
+        return self.__contract_abi
 
     @staticmethod
     def get_node_url():
--- socialNetwork/userManager/models.py
+++ socialNetwork/userManager/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bytes = models.CharField(max_length=66)
