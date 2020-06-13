2d2292657240304ddba7a27f1e810935eaeb9662
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-06-13 19:47:14 +0200

    added wrapper tests

---
+++ socialNetwork/userManager/test_blockchain_wrapper.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from .blockchain_wrapper import BlockchainWrapper
+import json
+
+
+class BlockchainWrapperTests(TestCase):
+
+    def setUp(self):
+        """Set up the wrapper"""
+        self.wrapper = BlockchainWrapper()
+        self.assertEqual("http://127.0.0.1:8545", self.wrapper.get_node_url())
+
+    def test_get_address(self):
+        """Get the contract address"""
+        try:
+            with open("../blockchain/build/contracts/SocialNetwork-address", 'r', encoding='utf-8') as f:
+                info_address = f.read()
+        except IOError:
+            raise IOError("Error reading contract address")
+
+        self.assertEqual(info_address, self.wrapper.get_contract_address())
+
+    def test_get_abi(self):
+        """Get the contract abi"""
+        try:
+            with open("../blockchain/build/contracts/SocialNetwork.json", 'r', encoding='utf-8') as abi:
+                info_json = json.load(abi)
+        except IOError:
+            raise IOError("Error reading contract ABI")
+
+        self.assertEqual(info_json["abi"], self.wrapper.get_contract_abi())
+
+    def test_get_bytes(self):
+        """Get user bytes for "user1" """
+        self.assertEqual("0xa1c2b8080ed4b6f56211e0295659ef87dd454b0a884198c10384f230525d4ee8",
+                         self.wrapper.get_user_bytes("user1"))
+
+
--- socialNetwork/userManager/tests.py
+++ socialNetwork/userManager/tests.py
@@ -2,15 +2,14 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Users
 
-# Create your tests here.
 
 class UserModelTests(TestCase):
 
     def test_new_user(self):
         """Creating Users model"""
-        auth_user = User.objects.create_user(username="danilo", email="", password="1234")
-        Users.objects.create(user=auth_user, bytes="0x0")
-        user_object = Users.objects.get(user=auth_user)
-        self.assertEqual(user_object.bytes, "0x0")
-        print(f"Test successful!")
+        user = User.objects.create_user(username="user1", email="test@test.com", password="1234")
+        Users.objects.create(user=user, bytes="0xa1c2b8080ed4b6f56211e0295659ef87dd454b0a884198c10384f230525d4ee8")
+
+        user_object = Users.objects.get(user=user)
+        self.assertEqual(user_object.bytes, "0xa1c2b8080ed4b6f56211e0295659ef87dd454b0a884198c10384f230525d4ee8")
 
