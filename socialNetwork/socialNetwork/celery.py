84e03526dc5152c354c41f86b7f9aaa727362cab
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-06-27 22:16:05 +0200

    added redis url to settings.py

--- socialNetwork/socialNetwork/celery.py
+++ socialNetwork/socialNetwork/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
-import os
 from celery import Celery
+from django.conf import settings
+import os
+
+REDIS_URL = settings.REDIS_URL
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialNetwork.settings')
@@ -8,5 +11,5 @@ app = Celery('socialNetwork')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.update(
-    BROKER_URL='redis://redis:6379',
+    BROKER_URL=REDIS_URL,
 )
