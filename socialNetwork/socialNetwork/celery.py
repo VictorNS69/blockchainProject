a78b956fe25ad4877c647621d3a6bce42a3d6e89
Víctor Nieves Sánchez <vnievess@gmail.com>
2020-06-27 22:31:21 +0200

    Revert "added redis url to settings.py"
    This reverts commit 84e03526dc5152c354c41f86b7f9aaa727362cab.

--- socialNetwork/socialNetwork/celery.py
+++ socialNetwork/socialNetwork/celery.py
@@ -1,9 +1,6 @@
 from __future__ import absolute_import, unicode_literals
-from celery import Celery
-from django.conf import settings
 import os
-
-REDIS_URL = settings.REDIS_URL
+from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialNetwork.settings')
@@ -11,5 +8,5 @@ app = Celery('socialNetwork')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.update(
-    BROKER_URL=REDIS_URL,
+    BROKER_URL='redis://redis:6379',
 )
